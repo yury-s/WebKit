f0b1144844bf6da1d9cc35f82e051c0647261686
playwright-devops <devops@playwright.dev>
2025-03-10 15:29:31 +0100

    chore(webkit): bootstrap build #2145

--- Source/WebKit/WebProcess/WebProcess.cpp
+++ Source/WebKit/WebProcess/WebProcess.cpp
@@ -91,6 +91,7 @@
 #include "WebsiteData.h"
 #include "WebsiteDataStoreParameters.h"
 #include "WebsiteDataType.h"
+#include <JavaScriptCore/IdentifiersFactory.h>
 #include <JavaScriptCore/JSLock.h>
 #include <JavaScriptCore/MemoryStatistics.h>
 #include <JavaScriptCore/WasmFaultSignalHandler.h>
@@ -382,6 +383,14 @@ void WebProcess::initializeProcess(const AuxiliaryProcessInitializationParameter
     {
         JSC::Options::AllowUnfinalizedAccessScope scope;
         JSC::Options::allowNonSPTagging() = false;
+        // Playwright begin
+        // SharedBufferArray is enabled only on Mac via XPC sercvice "enable-shared-array-buffer" option.
+        // For other platforms, enable it here.
+#if !PLATFORM(COCOA)
+        if (parameters.shouldEnableSharedArrayBuffer)
+            JSC::Options::useSharedArrayBuffer() = true;
+#endif
+        // Playwright end
         JSC::Options::notifyOptionsChanged();
     }
 
@@ -389,6 +398,8 @@ void WebProcess::initializeProcess(const AuxiliaryProcessInitializationParameter
     
     platformInitializeProcess(parameters);
     updateCPULimit();
+
+    Inspector::IdentifiersFactory::initializeWithProcessID(parameters.processIdentifier->toUInt64());
 }
 
 void WebProcess::initializeConnection(IPC::Connection* connection)
@@ -968,6 +979,8 @@ void WebProcess::createWebPage(PageIdentifier pageID, WebPageCreationParameters&
 
     if (m_shouldSuppressHDR)
         RefPtr { result.iterator->value }->setShouldSuppressHDR(m_shouldSuppressHDR);
+
+    result.iterator->value->didAddWebPageToWebProcess();
 }
 
 void WebProcess::removeWebPage(PageIdentifier pageID)
