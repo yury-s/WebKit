2958595ef2711bcbbd583ebd7a9b2389f6198579
playwright-devops <devops@playwright.dev>
2025-03-24 22:56:39 -0700

    chore(webkit): bootstrap build #2152

--- Tools/WebKitTestRunner/TestController.cpp
+++ Tools/WebKitTestRunner/TestController.cpp
@@ -690,6 +690,7 @@ PlatformWebView* TestController::createOtherPlatformWebView(PlatformWebView* par
         nullptr, // requestStorageAccessConfirm
         nullptr, // shouldAllowDeviceOrientationAndMotionAccess
         nullptr, // runWebAuthenticationPanel
+        0,       // handleJavaScriptDialog
         nullptr, // decidePolicyForSpeechRecognitionPermissionRequest
         nullptr, // decidePolicyForMediaKeySystemPermissionRequest
         nullptr, // queryPermission
@@ -1165,6 +1166,7 @@ void TestController::createWebViewWithOptions(const TestOptions& options)
         nullptr, // requestStorageAccessConfirm
         shouldAllowDeviceOrientationAndMotionAccess,
         runWebAuthenticationPanel,
+        0, // handleJavaScriptDialog
         nullptr, // decidePolicyForSpeechRecognitionPermissionRequest
         decidePolicyForMediaKeySystemPermissionRequest,
         queryPermission,
