1c90b06da9cf52ace4d0da42dfcd77bf4e69ac7e
Diego Pino Garcia <dpino@igalia.com>
2025-11-03 10:10:34 +0100

    Resolve conflicts

--- Source/WebKit/UIProcess/RemotePageProxy.cpp
+++ Source/WebKit/UIProcess/RemotePageProxy.cpp
@@ -37,12 +37,8 @@
 #include "ProvisionalFrameProxy.h"
 #include "RemotePageDrawingAreaProxy.h"
 #include "RemotePageFullscreenManagerProxy.h"
-<<<<<<< HEAD
-#include "RemotePageScreenOrientationManagerProxy.h"
-||||||| parent of 252c108b27c6 (chore(webkit): bootstrap build #2227)
-=======
 #include "RemotePagePlaybackSessionManagerProxy.h"
->>>>>>> 252c108b27c6 (chore(webkit): bootstrap build #2227)
+#include "RemotePageScreenOrientationManagerProxy.h"
 #include "RemotePageVisitedLinkStoreRegistration.h"
 #include "UserMediaProcessManager.h"
 #include "WebBackForwardList.h"
